7f7b13bbfd23aa8ba8732101520ffec74613775a
Jonathan Wang <jonathan.wang1996@gmail.com>
2020-11-13 22:22:40 -0600

    initial commit

---
+++ client/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Listings } from './sections';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import reportWebVitals from './reportWebVitals';
+import './styles/index.css';
+
+const client = new ApolloClient({
+  uri: '/api',
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <Listings title="TinyHouse Listings" />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
