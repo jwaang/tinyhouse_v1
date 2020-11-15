f536089a7a122adf1f97ea255d0006e3b38de54d
Jonathan Wang <jonathan.wang1996@gmail.com>
2020-11-14 23:13:15 -0600

    login/logout functionality with google oauth

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import { Home, Host, Listing, Listings, NotFound, User } from './sections';
+import { AppHeader, Home, Host, Listing, Login, Listings, NotFound, User } from './sections';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import reportWebVitals from './reportWebVitals';
+import { Affix, Layout } from 'antd';
+import { Viewer } from './lib/types';
 import './styles/index.css';
 
 const client = new ApolloClient({
   uri: '/api',
 });
 
+const initialviewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false
+}
+
 const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialviewer);
+
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/host" component={Host} />
-        <Route exact path="/listing/:id" component={Listing} />
-        <Route exact path="/listings/:location?" component={Listings} />
-        <Route exact path="/user/:id" component={User} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id="app">
+        <Affix offsetTop={0} className="app__affix-header">
+          <AppHeader viewer={viewer} setViewer={setViewer} />
+        </Affix>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route
+            exact
+            path="/login"
+            render={props => <Login {...props} setViewer={setViewer} />}
+          />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   )
 }
