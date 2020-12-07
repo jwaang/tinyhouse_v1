5209bdcf27854b33c17fc80cb1eee54a67a4b4c8
Jonathan Wang <jonathan.wang1996@gmail.com>
2020-12-07 14:28:07 -0600

    geocoding - not working

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,81 +1,89 @@
-import { gql } from 'apollo-server-express';
+import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
-    type Booking {
-        id: ID!
-        listing: Listing!
-        tenant: User!
-        checkIn: String!
-        checkOut: String!    
-    }
+	type Booking {
+		id: ID!
+		listing: Listing!
+		tenant: User!
+		checkIn: String!
+		checkOut: String!
+	}
 
-    type Bookings {
-        total: Int!
-        result: [Booking!]!
-    }
+	type Bookings {
+		total: Int!
+		result: [Booking!]!
+	}
 
-    enum ListingType {
-        APARTMENT
-        HOUSE
-    }
+	enum ListingType {
+		APARTMENT
+		HOUSE
+	}
 
-    enum ListingsFilter {
-        PRICE_LOW_TO_HIGH
-        PRICE_HIGH_TO_LOW
-    }
+	enum ListingsFilter {
+		PRICE_LOW_TO_HIGH
+		PRICE_HIGH_TO_LOW
+	}
 
-    type Listing {
-        id: ID!
-        title: String!
-        description: String!
-        image: String!
-        host: User!
-        type: ListingType!
-        address: String!
-        city: String!
-        bookings(limit: Int!, page: Int!): Bookings
-        bookingsIndex: String!
-        price: Int!
-        numOfGuests: Int!
-    }
+	type Listing {
+		id: ID!
+		title: String!
+		description: String!
+		image: String!
+		host: User!
+		type: ListingType!
+		address: String!
+        country: String!
+        admin: String!
+		city: String!
+		bookings(limit: Int!, page: Int!): Bookings
+		bookingsIndex: String!
+		price: Int!
+		numOfGuests: Int!
+	}
 
-    type Listings {
-        total: Int!
-        result: [Listing!]!
-    }
+	type Listings {
+        region: String
+		total: Int!
+		result: [Listing!]!
+	}
 
-    type User {
-        id: ID!
-        name: String!
-        avatar: String!
-        contact: String!
-        hasWallet: Boolean!
-        income: Int
-        bookings(limit: Int!, page: Int!): Bookings
-        listings(limit: Int!, page: Int!): Listings!
-    }
+	type User {
+		id: ID!
+		name: String!
+		avatar: String!
+		contact: String!
+		hasWallet: Boolean!
+		income: Int
+		bookings(limit: Int!, page: Int!): Bookings
+		listings(limit: Int!, page: Int!): Listings!
+	}
 
-    type Viewer {
-        id: ID 
-        token: String
-        avatar: String
-        hasWallet: Boolean
-        didRequest: Boolean!
-    }
+	type Viewer {
+		id: ID
+		token: String
+		avatar: String
+		hasWallet: Boolean
+		didRequest: Boolean!
+	}
 
-    input LogInInput {
-        code: String!
-    }
+	input LogInInput {
+		code: String!
+	}
 
-    type Query {
-        authUrl: String!
-        user(id: ID!): User!
-        listing(id: ID!): Listing!
-        listings(filter: ListingsFilter!, limit: Int!, page: Int!): Listings!
-    }
+	type Query {
+		authUrl: String!
+		user(id: ID!): User!
+		listing(id: ID!): Listing!
+		listings(
+			location: String
+			filter: ListingsFilter!
+			limit: Int!
+			page: Int!
+		): Listings!
+	}
 
-    type Mutation {
-        logIn(input: LogInInput): Viewer!
-        logOut: Viewer!
-    }
+	type Mutation {
+		logIn(input: LogInInput): Viewer!
+		logOut: Viewer!
+	}
 `;
