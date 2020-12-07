102fa3dbe647f0e18d2389aebdee75459c82937f
Jonathan Wang <jonathan.wang1996@gmail.com>
2020-12-07 15:46:43 -0600

    fixed geocode api issue... had to update api key

--- server/src/lib/api/Google.ts
+++ server/src/lib/api/Google.ts
@@ -1,10 +1,6 @@
 import { google } from "googleapis";
-import {
-	Client,
-	AddressComponent,
-	AddressType,
-	GeocodingAddressComponentType,
-} from "@googlemaps/google-maps-services-js";
+import { Client } from "@googlemaps/google-maps-services-js";
+import { AddressComponent, AddressType, GeocodingAddressComponentType } from "@googlemaps/google-maps-services-js/dist/common";
 
 const auth = new google.auth.OAuth2(
 	process.env.G_CLIENT_ID,
@@ -36,13 +32,15 @@ const parseAddress = (addressComponents: AddressComponent[]) => {
 		}
 	}
 
-	return { country, admin, city };
+	return {
+		country,
+		admin,
+		city,
+	};
 };
 
 export const Google = {
 	authUrl: auth.generateAuthUrl({
-		// eslint-disable-next-line @typescript-eslint/camelcase
-		access_type: "online",
 		scope: [
 			"https://www.googleapis.com/auth/userinfo.email",
 			"https://www.googleapis.com/auth/userinfo.profile",
@@ -50,7 +48,6 @@ export const Google = {
 	}),
 	logIn: async (code: string) => {
 		const { tokens } = await auth.getToken(code);
-
 		auth.setCredentials(tokens);
 
 		const { data } = await google
@@ -62,18 +59,19 @@ export const Google = {
 
 		return { user: data };
 	},
-	geocode: async (address: string) => {
-		if (!process.env.G_GEOCODE_KEY)
-			throw new Error("missing Google Maps API key");
 
-		const res = await maps.geocode({
-			params: { address, key: process.env.G_GEOCODE_KEY },
+	geocode: async (address: string) => {
+		const response = await maps.geocode({
+			params: {
+				address,
+				key: `${process.env.G_GEOCODE_KEY}`,
+			},
 		});
 
-		if (res.status < 200 || res.status > 299) {
+		if (response.status < 200 || response.status > 299) {
 			throw new Error("failed to geocode address");
 		}
 
-		return parseAddress(res.data.results[0].address_components);
+		return parseAddress(response.data.results[0].address_components);
 	},
 };
